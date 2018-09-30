a3bc10ab87124bd5cc9b853cf4fe90675fcf4e25
name <nata.mozhaueva@gmail.com>
2018-09-30 21:01:13 +0300

    +

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+	let result = 0;
+	let i = 1;  
+	while (Math.floor(number / Math.pow(5, i)) > 0) {
+ 		result += Math.floor(number / Math.pow(5, i));
+    	i++;
+ 	}
+ 	return result;
 }
